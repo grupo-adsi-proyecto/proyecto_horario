f0d5318b0eaf61cb14481d86872e7b9123ce97fb
kilordgosth <h.a.v.m@misena.edu.co>
2019-10-27 07:00:01 -0500

    .

--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,7 @@
 <header>
 	<ul>
 		<BR>
-  <a ><h1 font face="Verdana" ><font color="#238276"><b>OFTWARE GESTIÓN DE HORARIOS DEL C.D.M<h1></b></a></font>
+  <a ><h1 font face="Verdana" ><font color="#238276"><b>SOFTWARE GESTIÓN DE HORARIOS DEL C.D.M<h1></b></a></font>
         </BR>
     </ul>
 </header>
