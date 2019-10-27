eb8e80f19a6327b962fc66261de4b0f40eac0490
kilordgosth <h.a.v.m@misena.edu.co>
2019-10-27 07:01:08 -0500

    .

---
+++ conexion.php
@@ -0,0 +1,19 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db81";
+
+
+// Create connection
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+$conn ->set_charset("utf8");
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+?>
