989a0e492bafab8e743e0be1350599537b9407f3
kilordgosth <h.a.v.m@misena.edu.co>
2019-10-27 07:00:59 -0500

    .

--- conection.php
+++ conection.php
@@ -3,11 +3,13 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "definitiva2";
+$dbname = "DB70";
 
 
 // Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
+$conn ->set_charset("utf8");
 
 // Check connection
 if ($conn->connect_error) {
